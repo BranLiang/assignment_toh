edf18ff498669e0578c9b0537068bde925dcca4f
Bran Liang <lby89757@hotmail.com>
2016-07-23 20:21:21 +0800

    add more disk ability

--- towers.rb
+++ towers.rb
@@ -10,9 +10,9 @@
 
 class Game
   attr_accessor :col_1, :col_2, :col_3
-  def initialize
-    @num_disks  = 4
-    @ini_arrange = Array(1..4)
+  def initialize num_disks
+    @num_disks  = num_disks
+    @ini_arrange = Array(1..num_disks)
     @col_1 = @ini_arrange
     @col_2 = []
     @col_3 = []
@@ -58,7 +58,7 @@ class Game
 
   def array_fill arr
     result = []
-    4.times do |i|
+    @num_disks.times do |i|
       if arr[i] != nil
         result << arr[i]
       else
@@ -69,15 +69,16 @@ class Game
   end
 
   def display_one_line n
+    move_distance = @num_disks + 2
     unless n.nil?
-      ("=" * n).rjust(6) + "|" + ("=" * n).ljust(6)
+      ("=" * n).rjust(move_distance) + "|" + ("=" * n).ljust(move_distance)
     else
-      " ".rjust(6) + "|" + " ".ljust(6)
+      " ".rjust(move_distance) + "|" + " ".ljust(move_distance)
     end
   end
 
   def display_all
-    4.times do |i|
+    @num_disks.times do |i|
       print display_one_line array_fill(col_1).sort[i]
       print display_one_line array_fill(col_2).sort[i]
       print display_one_line array_fill(col_3).sort[i]
@@ -87,7 +88,9 @@ class Game
 end
 
 def game_start
-  new_game = Game.new
+  puts "How many disks do you wish for the game? (maximum 10 for the god sake)"
+  num_disks_input = gets.chomp.to_i
+  new_game = Game.new num_disks_input
   puts "Type in the format '1' click enter and '2' click enter"
   puts "to move the disk from column 1 to column 2."
   while !new_game.win?
@@ -98,7 +101,12 @@ def game_start
     break if a == 'q'
     b = gets.chomp
     break if b == 'q'
-    input << a.to_i << b.to_i
+    if a.to_i.between?(1, 3) && b.to_i.between?(1, 3)
+      input << a.to_i << b.to_i
+    else
+      puts "invaid input sorry"
+      next
+    end
     if input == 'q'
       break
     elsif !!(/\[\d.*\,.*\d\]/.match(input.to_s))
@@ -109,6 +117,7 @@ def game_start
   end
   game_over
 end
+
 
 puts "Welcome to Tower of Hanoi!"
 puts "If you want to start the game"
